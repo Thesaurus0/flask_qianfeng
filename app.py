af2242f162644a4fd316325d2d5cd904e7fd09d6
Thesaurus0 <wdk1231@163.com>
2019-12-08 08:55:52 +0800

    add click

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
-from flask import Flask
+import click
 
-app = Flask(__name__)
+from App import create_app
+
+app = create_app()
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@click.command('run-ser')
+@click.option('--port', type=click.INT, help='port number', default=8080, prompt='please provide port number:')
+@click.option('--debug', type=click.BOOL, help='debug mode', default=True, prompt='debug or not:')
+def run_server(port, debug):
+    app.run(port=port, debug=debug)
 
 
 if __name__ == '__main__':
-    app.run()
+    run_server()
