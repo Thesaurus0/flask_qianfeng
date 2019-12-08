04f79b7b6737476a68d920b6a2b2e2f3e6418a2d
Thesaurus0 <wdk1231@163.com>
2019-12-08 23:08:20 +0800

    add delete

--- App/view.py
+++ App/view.py
@@ -2,7 +2,9 @@ from flask import Blueprint, render_template
 
 blue = Blueprint('blue01', __name__)
 
-from App.model import Person
+from App.model import Person, Student
+from App.ext import  db
+
 
 def init_view(app):
     app.register_blueprint(blue)
@@ -11,3 +13,19 @@ def init_view(app):
 @blue.route('/hello/')
 def hello_world():
     return render_template('index.html')
+
+@blue.route('/addstu/')
+def add_student():
+    stu = Student()
+    stu.name = 'haha'
+    db.session.add(stu)
+    db.session.commit()
+    return 'sucess'
+
+
+@blue.route('/deletestu/<int:id>/')
+def delete_stu(id):
+    stu = Student.query.get(id)
+    db.session.delete(stu)
+    db.session.commit()
+    return 'delete done.'
